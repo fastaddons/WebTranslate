d1977256b49584d33fa8a9e8419c3c9e59196baa
Juraj Mäsiar <juraj.masiar@gmail.com>
2020-10-22 23:07:29 +0200

    - messages files

---
+++ locales/messages.el
@@ -0,0 +1,169 @@
+#               Translate
+o_tab_translate=Μεταφράζω
+
+#                 Google Translate
+translator_GOOGLE=Google Translate
+#                    Google 翻译
+translator_GOOGLE_CN=Google 翻译
+#               Microsoft Translator
+translator_BING=Microsoft Translator
+#                百度翻译 (Baidu Translator)
+translator_BAIDU=百度翻译 (Baidu Translator)
+
+#         Modal window
+open_in_0=Παράθυρο modal
+#         Popup window
+open_in_1=Αναδυόμενο παράθυρο
+#         New tab
+open_in_2=Νέα καρτέλα
+#         New window
+open_in_3=Νέο παράθυρο
+
+#            Modal window
+modal_window=Παράθυρο modal
+#                   Toolbar icon action
+toolbar_icon_action=Ενέργεια εικονίδιο γραμμής εργαλείων
+
+#                    Translation provider
+translation_provider=Πάροχος μετάφρασης
+#            Translate to:
+translate_to=Μετάφραση σε:
+#          No overlay (the dark area around)
+no_overlay=Χωρίς επικάλυψη (η σκοτεινή περιοχή γύρω)
+#               Modal window will stay opened even if you click outside.
+no_overlay_hint=Το παράθυρο του modal θα παραμείνει ανοιχτό ακόμα κι αν κάνετε κλικ έξω.
+#x                 Open translator in:
+open_translator_in=
+#                   Open translation in:
+open_translation_in=Άνοιγμα μετάφρασης σε:
+#          Fallback method
+fallback_1=Εφεδρική μέθοδος
+#          Fallback method
+fallback_2=Εφεδρική μέθοδος
+#              Modal window and popup window can't be used in some cases.
+fallback_title=Το παράθυρο Modal και το αναδυόμενο παράθυρο δεν μπορούν να χρησιμοποιηθούν σε ορισμένες περιπτώσεις.
+#           Open in new tab
+open_in_tab=Άνοιγμα σε νέα καρτέλα
+
+#                   Translate this page
+translate_this_page=Μεταφράστε αυτήν τη σελίδα
+#                   Translate "$1"
+selection_translate=Μετάφραση "$1"
+#              Translate page
+page_translate=Μετάφραση σελίδας
+
+#             selected text
+selected_text=επιλεγμένο κείμενο
+
+#                    Disable context menu: "$1"
+disable_context_menu=Απενεργοποίηση μενού περιβάλλοντος: "$1"
+
+
+
+#        Web Translate
+app_name=Web Translate
+#        "Web Translate" is easy to use translator for your Web browser / Thunderbird!
+app_desc=Το "Web Translate" είναι εύκολο στη χρήση μεταφραστή για το πρόγραμμα περιήγησης Web / Thunderbird
+
+
+#               Import finished!
+import_finished=Η εισαγωγή ολοκληρώθηκε!
+#            Error
+import_error=Σφάλμα
+#                  Import backup file $1
+import_backup_file=Εισαγωγή εφεδρικού αρχείου $1
+
+
+#             Search
+search_search=Αναζήτηση
+
+
+#        Homepage
+homepage=Αρχική σελίδα
+#          Addon page
+addon_page=Σελίδα Addon
+
+
+#               Font size
+label_font_size=Μέγεθος γραμματοσειράς
+#          Next
+label_next=Επόμενο
+#          Note:
+label_note=Σημείωση:
+#            Remove
+label_remove=Αφαιρώ
+#             WARNING:
+label_warning=ΠΡΟΕΙΔΟΠΟΙΗΣΗ:
+#             Default:
+label_default=Προεπιλογή:
+#          Done!
+label_done=Ολοκληρώθηκε!
+#            Copied!
+label_copied=Αντιγράφηκε!
+#           Close
+label_close=Κλείσιμο
+#               Open page
+label_open_page=Άνοιγμα σελίδας
+
+
+#           Agree
+label_agree=Έγκριση
+#            Cancel
+label_cancel=Ακύρωση
+
+
+#             Help this project
+op_support_me=Βοηθήστε αυτό το έργο
+#                 Support independent developer!
+support_developer=Υποστήριξη ανεξάρτητου προγραμματιστή!
+#           Hello there!
+hello_there=Γειά σου!
+#        I'm addon developer since 2016 and my goal is to build powerful extensions for your web-browser to make you more productive.
+about_me=Είμαι προγραμματιστής πρόσθετων από το 2016 και στόχος μου είναι να δημιουργήσω ισχυρές επεκτάσεις για το πρόγραμμα περιήγησης ιστού που θα σας κάνουν πιο παραγωγικούς.
+#              If you think I'm doing a good job, please donate :)
+about_good_job=Αν νομίζετε ότι κάνω καλή δουλειά, δώστε :)
+#            Your donation is my only income!
+about_income=Η δωρεά σας είναι το μόνο εισόδημά μου!
+#            Thank you for supporting my work!
+about_thanks=Σας ευχαριστώ που υποστηρίξατε τη δουλειά μου!
+
+#                  Context menu
+o_tab_context_menu=Μενού περιβάλλοντος
+#           About
+o_tab_about=Σχετικά
+#              Settings
+o_tab_settings=Ρυθμίσεις
+
+#        pixels
+o_pixels=εικονοκύτταρα
+#         seconds
+o_seconds=δευτερόλεπτα
+
+#        or
+label_or=ή
+
+#             Hello :)
+o_about_hello=Γεια σας :)
+#             My name is Juraj Mäsiar and I'm the author of $1.
+o_about_intro=Το όνομά μου είναι ο Juraj Mäsiar και είμαι ο συντάκτης του $1.
+#               If you like it, please rate it here:
+o_about_like_it=Εάν σας αρέσει, παρακαλώ βαθμολογήστε το εδώ:
+#              Thank you!
+o_about_thanks=Σας ευχαριστώ!
+#               Contact:
+o_about_contact=Επικοινωνία:
+#              Report issues:
+o_about_issues=Αναφορά ζητημάτων:
+#                    Social media:
+o_about_social_media=Μέσα κοινωνικής δικτύωσης:
+
+#             No matching results
+ps_no_results=Δεν υπάρχουν αποτελέσματα που να ταιριάζουν
+
+#             No data
+label_no_data=Χωρίς δεδομένα
+
+#         Dark mode
+dark_mode=Σκοτεινή λειτουργία
+#                Controlled by system
+dark_mode_system=Ελέγχεται από σύστημα
---
+++ locales/messages.es
@@ -0,0 +1,169 @@
+#               Translate
+o_tab_translate=Traducir
+
+#                 Google Translate
+translator_GOOGLE=Google Translate
+#                    Google 翻译
+translator_GOOGLE_CN=Google 翻译
+#               Microsoft Translator
+translator_BING=Microsoft Translator
+#                百度翻译 (Baidu Translator)
+translator_BAIDU=百度翻译 (Baidu Translator)
+
+#         Modal window
+open_in_0=Ventana modal
+#         Popup window
+open_in_1=Ventana emergente
+#         New tab
+open_in_2=Nueva pestaña
+#         New window
+open_in_3=Nueva ventana
+
+#            Modal window
+modal_window=Ventana modal
+#                   Toolbar icon action
+toolbar_icon_action=Acción del icono de la barra de herramientas
+
+#                    Translation provider
+translation_provider=Proveedor de traducción
+#            Translate to:
+translate_to=Traducir a:
+#          No overlay (the dark area around)
+no_overlay=Sin superposición (el área oscura alrededor)
+#               Modal window will stay opened even if you click outside.
+no_overlay_hint=La ventana modal permanecerá abierta incluso si hace clic fuera.
+#x                 Open translator in:
+open_translator_in=
+#                   Open translation in:
+open_translation_in=Traducción abierta en:
+#          Fallback method
+fallback_1=Método alternativo
+#          Fallback method
+fallback_2=Método alternativo
+#              Modal window and popup window can't be used in some cases.
+fallback_title=La ventana modal y la ventana emergente no se pueden utilizar en algunos casos.
+#           Open in new tab
+open_in_tab=Abrir en una pestaña nueva
+
+#                   Translate this page
+translate_this_page=Traduzca esta pagina
+#                   Translate "$1"
+selection_translate=Traducir "$1"
+#              Translate page
+page_translate=Traducir página
+
+#             selected text
+selected_text=texto seleccionado
+
+#                    Disable context menu: "$1"
+disable_context_menu=Deshabilitar el menú contextual: "$1"
+
+
+
+#        Web Translate
+app_name=Web Translate
+#        "Web Translate" is easy to use translator for your Web browser / Thunderbird!
+app_desc=¡"Web Translate" es un traductor fácil de usar para su navegador web / Thunderbird!
+
+
+#               Import finished!
+import_finished=¡Restauración finalizada!
+#            Error
+import_error=Error
+#                  Import backup file $1
+import_backup_file=Importar archivo de respaldo $1
+
+
+#             Search
+search_search=Buscar
+
+
+#        Homepage
+homepage=Página principal
+#          Addon page
+addon_page=Página de complementos
+
+
+#               Font size
+label_font_size=Tamaño de fuente
+#          Next
+label_next=Siguiente
+#          Note:
+label_note=Nota:
+#            Remove
+label_remove=Eliminar
+#             WARNING:
+label_warning=ATENCIÓN:
+#             Default:
+label_default=Por defecto:
+#          Done!
+label_done=¡Hecho!
+#            Copied!
+label_copied=¡Copiado!
+#           Close
+label_close=Cerrar
+#               Open page
+label_open_page=Abrir página
+
+
+#           Agree
+label_agree=Acepto
+#            Cancel
+label_cancel=Cancelar
+
+
+#             Help this project
+op_support_me=Apoya este proyecto
+#                 Support independent developer!
+support_developer=¡Apoye al desarrollador independiente!
+#           Hello there!
+hello_there=¡Hola a todos!
+#        I'm addon developer since 2016 and my goal is to build powerful extensions for your web-browser to make you more productive.
+about_me=Soy desarrollador de complementos desde 2016 y mi objetivo es crear potentes extensiones para su navegador web para que sea más productivo.
+#              If you think I'm doing a good job, please donate :)
+about_good_job=Si crees que estoy haciendo un buen trabajo, haz una donación :)
+#            Your donation is my only income!
+about_income=¡Tu donación es mi único ingreso!
+#            Thank you for supporting my work!
+about_thanks=¡Gracias por apoyar mi trabajo!
+
+#                  Context menu
+o_tab_context_menu=Menú contextual
+#           About
+o_tab_about=Acerca de
+#              Settings
+o_tab_settings=Configuraciones
+
+#        pixels
+o_pixels=píxeles
+#         seconds
+o_seconds=segundos
+
+#        or
+label_or=o
+
+#             Hello :)
+o_about_hello=Hola :)
+#             My name is Juraj Mäsiar and I'm the author of $1.
+o_about_intro=Me llamo Juraj Mäsiar y soy el creador de $1.
+#               If you like it, please rate it here:
+o_about_like_it=Si te gusta, por favor valóralo aquí:
+#              Thank you!
+o_about_thanks=¡Muchas gracias!
+#               Contact:
+o_about_contact=Contacto:
+#              Report issues:
+o_about_issues=Informar problemas:
+#                    Social media:
+o_about_social_media=Redes sociales:
+
+#             No matching results
+ps_no_results=No hay resultados coincidentes
+
+#             No data
+label_no_data=Sin datos
+
+#         Dark mode
+dark_mode=Modo oscuro
+#                Controlled by system
+dark_mode_system=Controlado por el sistema
